1720641af8e8cff349e86d525da0c1c81a97470d
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-04-24 22:21:46 +0200

    Basic SeriLog example

---
+++ SeriLog.Net5.CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SeriLog.Net5.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = ServiceConfigurator
+                .CreateHostBuilder(args)
+                .Build();
+
+            var logger = host.Services.GetService<ILogger<Program>>();
+
+            logger.LogInformation("Hello world");
+        }
+    }
+}
