db569c38a39f9a9e5aedb07dd7b295f966729855
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2022-04-25 22:15:29 +0200

    Serilog CLI

--- SeriLog.Net5.CLI/Program.cs
+++ SeriLog.Net5.CLI/Program.cs
@@ -1,19 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Serilog;
+using Serilog.Events;
+using System.Threading.Tasks;
 
 namespace SeriLog.Net5.CLI
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var host = ServiceConfigurator
-                .CreateHostBuilder(args)
-                .Build();
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.ApplicationInsights(new TelemetryConfiguration { InstrumentationKey = "ff75007e-f994-43e2-918f-971fa6afbe03"}, TelemetryConverter.Traces)
+                .WriteTo.Console()
+                .CreateLogger();
 
-            var logger = host.Services.GetService<ILogger<Program>>();
+            var counter = 1;
+            do
+            {
+                log.Information("Hello world loop {counter}", counter);
+                counter++;
 
-            logger.LogInformation("Hello world");
+                await Task.Delay(2500);
+            }while (true);
+            
         }
     }
 }
